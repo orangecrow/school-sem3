6c0f159c6941fe2db2a8c33df9603f27931ce367
orangecrow <wynalazca47@gmail.com>
2019-11-30 18:37:32 +0100

    most done?

--- sequence.cpp
+++ sequence.cpp
@@ -1,9 +1,40 @@
-#include<sequence.h> template<typename val_type>
-sequence<val_type>::~sequance(){
-	node *ptr;
-	ptr = head;
-	while(ptr=!NULL){
-		ptr=ptr->next;	
-	}
+#include<sequence.h> 
+//template<typename val_type>
+node* sequence::ad(val_type a, node* np){
+	if (np == NULL)
+		return np;
+	node* tmp = np->next;
+	np->next = new node;
+	np->next->value = a;
+	np->next->next = tmp;
+	return np;
 }
-
+node* sequence::del(node* np){
+	if (np == NULL)
+		return np;
+	node* del=np->next;
+	if (np->next!=NULL){
+		np->next=np->next->next;
+		delete del;
+	}
+	return np;
+}
+node* sequence::delal(){
+	while(del(*head)->next!=NULL);
+	return *head;
+}
+int sequence::recursive_copy(node* a){
+	if(a==NULL)
+		return 0;
+	recursive_copy(a->next);
+	ad(a->value,*head);
+	return 0;
+}
+sequence::sequence(sequence& a){
+		recursive_copy(&(a.head))
+}
+sequence sequence::operator=(sequence& a){
+	if (this==&a)
+		return;
+	
+}
--- sequence.h
+++ sequence.h
@@ -1,12 +1,27 @@
-struct node{
-	val_type value;
-	node *next;
-}
-template <typename val_type>
+//template <typename val_type>
+typedef int val_type;
 class sequence{
 private:
-	node *head;
+	struct node{
+		val_type value;
+		node *next;
+	}
+	node head;
+	int recursive_copy(node*);
 public:
-	sequance(){head=NULL;}
-	~sequance();
+	sequence(val_type);
+	sequence(sequence&);
+	sequence operator=(sequence&);
+	~sequence();
+	node* ad(val_type, node*);// adds node after the one that is pointed to by the pointer
+	node* del(node*);// deletes a node after the one that is pointed to by the pointer
+	node* delal();//deletes all elements
 }
+	/*struct iterator{
+		node* curr;
+		node* prev;
+		iterator operator++();
+		iterator operator++(int);
+		node& operator*();
+		iterator(){curr=head; prev=NULL}
+	}*/
