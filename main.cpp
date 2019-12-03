691262696f8e759a382c07ccd32c91bc40b76c54
orangecrow <wynalazca47@gmail.com>
2019-12-03 16:52:12 +0100

    main added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+#include"sequence.h"
+#include"sequence.cpp"
+
 int main(){
-	int count_words(counter<string>&, istream& source);
+	Sequence a;
+	Sequence b = a;
+	a=a;
+	for(int i=0;i<12;++i)
+		a.ad(i);
+	a.pnal();
+	b=a;
+	a.delal();
+	Sequence* c= new Sequence;
+	c->ad(100);
+	b.pnal();
+	c->pnal();
+	a.pnal();
 }
+
+/*
+int count_words(counter<string>& cnt, istream& source){
+return 0;
+}
+*/
