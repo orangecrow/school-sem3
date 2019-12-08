fd9a3fb24073008c9a6fb1b0bb0ad634b18f4d77
orangecrow <wynalazca47@gmail.com>
2019-12-08 20:32:57 +0100

    main fix

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int itest() {
 }
 
 
-int test(){
+int ctest(){
 	Counter<std::string> a;
 	Counter<std::string> b = a;
 	a=a;
@@ -34,6 +34,23 @@ int test(){
 	a.usr_del();
 	Counter<std::string>* c= new Counter<std::string>;
 	c->specialad("qewr");
+	c->pnal();
+	b.pnal();
+	c->pnal();
+	a.pnal();
+	delete c;
+}
+int stest(){
+	Sequence<std::string> a;
+	Sequence<std::string> b = a;
+	a=a;
+	for(int i=0;i<2;++i)
+		a.ad("asdf");
+	a.pnal();
+	b=a;
+	a.usr_del();
+	Sequence<std::string>* c= new Sequence<std::string>;
+	c->ad("qewr");
 	c->pnal();
 	b.pnal();
 	c->pnal();
